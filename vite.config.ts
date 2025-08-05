33af7390ffab018d65bbc33b693008329762fb46
Sadiqshaik97 <shaiksadiq0392@gmail.com>
2025-08-05 20:00:05 +0530

    sadiqdapp

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   build: {
+//     outDir: "dist",
+//   },
+//   server: {
+//     open: true,
+//   },
+//   plugins: [
+//     react(),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./frontend"),
+//     },
+//   },
+// });
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    outDir: "dist",
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["buffer"],
   },
-  server: {
-    open: true,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./frontend"),
-    },
-  },
-});
+})
